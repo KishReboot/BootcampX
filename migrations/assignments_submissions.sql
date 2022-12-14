1613c01dd38397bb440175db2f228ea4fd7acac1
KishReboot <russellsalopek@pm.me>
2022-12-13 21:28:17 -0500

    Small edit to keep formatting similar

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE assignments (
-  id serial PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   content TEXT,
   day INTEGER,
@@ -8,7 +8,7 @@ CREATE TABLE assignments (
 );
 
 CREATE TABLE assignment_submissions (
-  id serial PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
